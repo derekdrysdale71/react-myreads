cbb1ff9b568e788e027648bc4815e1594e80c796
Derek <derek@darthvader.local>
2017-12-01 11:14:50 -0800

    installed prop-types

--- src/App.js
+++ src/App.js
@@ -1,5 +1,6 @@
 import React, { Component } from 'react'
 import { Route } from 'react-router-dom'
+import BookShelf from './components/BookShelf'
 import * as BooksAPI from './BooksAPI'
 import './App.css'
 
--- src/Components/BookShelf.js
+++ src/Components/BookShelf.js
@@ -1,8 +1,18 @@
 import React, { Component } from 'react'
+import Book from './Book'
 
 class BookShelf extends Component {
   Render() {
-
+    <div className="bookshelf">
+      <h2 className="bookshelf-title">{title}</h2>
+      <div className="bookshelf-books">
+        <ol className="books-grid">
+          {books.map((book) => (
+            <Book key={book.id} book={book} onChangeShelf={onChangeShelf} />
+          ))}
+        </ol>
+      </div>
+    </div>
   }
 }
 
