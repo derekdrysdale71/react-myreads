2c5efc74294e3e646a08a86071c61b0e9ac86181
Derek <derek@darthvader.local>
2017-12-05 17:28:17 -0800

    fixed issue where all authors were not being listed

--- src/Components/Book.js
+++ src/Components/Book.js
@@ -20,7 +20,7 @@ class Book extends Component {
             <ShelfSelector book={book} onChangeShelf={onChangeShelf}/>
           </div>
           <div className="book-title">{ book.title }</div>
-          <div className="book-authors">{ book.authors }</div>
+          <div className="book-authors">{ book.authors ? book.authors.join(', ') : '' }</div>
         </div>
       </li>
     )
